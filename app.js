4f3be617f7edf877d01e3f95c0dc984b5cc6ebc9
Nicky <51677808+Techgeek29@users.noreply.github.com>
2021-03-18 15:18:43 -0400

    Started the server

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-const app =require('express');
+const express =require('express');
 
+const app =express();
+
+const port =process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send ("Hello"));
+app.listen(port, () => console.log(`Server running on port ${port}`));
